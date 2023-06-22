6f21c270ac3dd8eec4d11398ede875d78e5a858e
softorres <101760901+softorres@users.noreply.github.com>
2023-06-22 02:03:59 -0400

    Update TSP.py

--- TSP.py
+++ TSP.py
@@ -71,33 +71,7 @@ def test_can_create_random_individual():
 
     assert sorted(individual.genes) == list(range(num_genes))      
      ###################################################   
-        
-City = namedtuple("City", ["x", "y"])
-
-
-def pairwise(iterable):
-    iterable = list(iterable)
-    return zip(iterable, iterable[1:] + iterable[:1])
-
-
-def random_interval(individual):
-    i, j = random.sample(range(len(individual)), k=2)
-    i, j = min(i, j), max(i, j)
-    return i, j
-
-
-def random_population(population_size, num_genes):
-    return [list(permutation(range(num_genes))) for _ in range(population_size)]
-
-
-def read_input(path):
-    cities = []
-    with open(path, "r") as input_file:
-        for line in input_file:
-            x, y = line.split(" ")
-            city = City(float(x), float(y))
-            cities.append(city)
-    return cities      
+  
         
         
         
@@ -232,6 +206,33 @@ def genetic_tsp(
     return best_solution, best_distance
 
 
+        
+City = namedtuple("City", ["x", "y"])
+
+
+def pairwise(iterable):
+    iterable = list(iterable)
+    return zip(iterable, iterable[1:] + iterable[:1])
+
+
+def random_interval(individual):
+    i, j = random.sample(range(len(individual)), k=2)
+    i, j = min(i, j), max(i, j)
+    return i, j
+
+
+def random_population(population_size, num_genes):
+    return [list(permutation(range(num_genes))) for _ in range(population_size)]
+
+
+def read_input(path):
+    cities = []
+    with open(path, "r") as input_file:
+        for line in input_file:
+            x, y = line.split(" ")
+            city = City(float(x), float(y))
+            cities.append(city)
+    return cities    
 ##########################################
 col1, col2 = st.columns(2)
 
